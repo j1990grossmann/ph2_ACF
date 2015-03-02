b839e6dac98e20b1b9a30662edbfea28a9a67e85
j1990grossmann <Johannes.Grossmann@oeaw.ac.at>
2015-03-02 15:58:51 +0100

    asdfasdf

---
+++ src/test.cc
@@ -0,0 +1,39 @@
+#include <cstring>
+#include <vector>
+#include <TApplication.h>
+#include "TROOT.h"
+#include "TGraphErrors.h"
+#include "TFile.h"
+#include <string>
+
+typedef std::map<uint8_t, std::vector<TGraphErrors*> > GraphMap;
+
+int main( int argc, char* argv[] )
+{
+	
+	TApplication cApp( "Root Application", &argc, argv );
+	gROOT->SetBatch( true );
+	TFile *file = new TFile("../testfile.root","RECREATE");
+	GraphMap lala;
+	std::vector<TGraphErrors*> peb;
+	for(int i=0; i<10;i++)
+	{
+		std::string depper;
+		depper+=i;
+		TGraphErrors *d = new TGraphErrors();
+		d->SetTitle(depper);
+		d->SetPoint(0,1,1);
+		
+		peb.push_back(d);
+	}
+	
+	lala[0]=peb;
+	for ( const auto& cGraph : lala)
+		for(const auto& graphsvector : cGraph.second)
+			graphsvector->Write(graphsvector->GetTitle());
+	
+	
+
+	std::cout<<"finish scans"<<std::endl;
+	return 0;
+}
