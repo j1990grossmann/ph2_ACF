01d3b16524c3fac2880ef74d6b39fc4f2e5ced8a
j1990grossmann <Johannes.Grossmann@oeaw.ac.at>
2015-03-09 13:37:59 +0100

    final measurement programm for u and i

--- tools/MuxTest.cc
+++ tools/MuxTest.cc
@@ -133,12 +133,10 @@ void MuxTest::ScanVplusAMux()
 	int i=0;
 	for(auto& amuxregisterpair : fTestRegisterVector )
 	{
-		InitializeSettings(pHWfile);
-		ConfigureHw();
 		std::cout<<BOLDBLUE<<"Scanning "<<amuxregisterpair.first<<" with Amux Register setting "<<amuxregisterpair.second<<"\t"<<i<<RESET<<endl;
 		
-// 		CbcMultiRegWriter cWriter2(fCbcInterface, fNominalValuesVector);
-// 		accept(cWriter2);
+		CbcMultiRegWriter cWriter2(fCbcInterface, fNominalValuesVector);
+		accept(cWriter2);
 		
 		CbcRegWriter cWriter1( fCbcInterface, "MiscTestPulseCtrl&AnalogMux", amuxregisterpair.second );
 		accept( cWriter1 );	
