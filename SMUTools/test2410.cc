9fd6d931bf382fe10a2e510dbf1a593285c4988f
j1990grossmann <Johannes.Grossmann@oeaw.ac.at>
2015-03-24 14:54:48 +0100

    sdf

---
+++ SMUTools/test2410.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <csignal>
+
+#include "Keithley2410.h"
+#include <vector>
+
+using namespace std;
+using namespace KEITHLEY2410;
+// using namespace HAMEG4040;
+using namespace boost;
+void signalHandler( int signum )
+{
+//     cout << "Interrupt signal (" << signum << ") received.\n";
+	
+    // cleanup and close up stuff here  
+    // terminate program  
+
+   exit(signum);  
+
+}
+
+int test2410(int argc, char* argv[])
+{
+	signal(SIGINT, signalHandler);  
+	
+	//File for data:
+// 	ofstream MeasFile;
+// 	MeasFile.open("./ResultsScan/Measurement.txt");
+	
+	KEITHLEY2410::Keithley2410 *k = new KEITHLEY2410::Keithley2410();
+	
+	string readstr;
+	k->Configure();
+	k->Outp(1);
+	k->Read(readstr);
+	cout<<readstr<<endl;
+	
+	k->SourVoltLev("10");
+	k->Read(readstr);
+	cout<<readstr<<endl;
+	
+
+	
+	delete k;
+
+	return 0;
+}
