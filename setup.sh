21709c58abd7f754cb824583e2fe2b779e174b5a
j1990grossmann <Johannes.Grossmann@oeaw.ac.at>
2016-02-22 10:14:00 +0100

    change makefiles such that binaries have different names, add conditions for general setup.sh to cope with Plugin directory

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DIRECTORY=Plugin
+
 #CACTUS
 export CACTUSBIN=/opt/cactus/bin
 export CACTUSLIB=/opt/cactus/lib
@@ -20,3 +22,8 @@ export ANTENNALIB=CMSPh2_AntennaDriver/lib
 
 export PATH=$BASE_DIR/bin:$PATH
 export LD_LIBRARY_PATH=$BASE_DIR/$ANTENNALIB:$BASE_DIR/RootWeb/lib:/opt/cactus/lib:$BASE_DIR/lib:${LD_LIBRARY_PATH}
+
+if [ -d "$BASE_DIR/$DIRECTORY" ]; then
+    printf "Plugin found\n Add LD_LIBRARY_PATH and PATH for Plugins\n"
+    source "$BASE_DIR/$DIRECTORY/setup.sh"
+fi
