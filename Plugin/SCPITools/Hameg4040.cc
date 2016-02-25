56ceaec34d6c84d5722a9974ea6b4604c8902849
j1990grossmann <Johannes.Grossmann@oeaw.ac.at>
2016-02-25 17:47:17 +0100

    test

--- Plugin/SCPITools/Hameg4040.cc
+++ Plugin/SCPITools/Hameg4040.cc
@@ -189,7 +189,7 @@ void Hameg4040::ReadSynchronized(string& command, string& read_str)
 {
 	int counter = 0;
 	this->serial.writeString(command+endline);
-	auto start = std::chrono::system_clock::now();
+// 	auto start = std::chrono::system_clock::now();
 	read_str="";
 	while(read_str.empty())
 	{		
@@ -199,9 +199,9 @@ void Hameg4040::ReadSynchronized(string& command, string& read_str)
 		// 		std:cout<<readstring<<"\ttest\t"<<counter<<std::endl;
 		counter++;
 	}
-	auto end = std::chrono::system_clock::now();
-	auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-	std:cout<<read_str<<"\ttest\t"<<counter<<"\telapsed\t"<<elapsed.count()<<std::endl;
+// 	auto end = std::chrono::system_clock::now();
+// 	auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+// 	std:cout<<read_str<<"\ttest\t"<<counter<<"\telapsed\t"<<elapsed.count()<<std::endl;
 }
 
 void Hameg4040::WriteSynchronized(string& command)
