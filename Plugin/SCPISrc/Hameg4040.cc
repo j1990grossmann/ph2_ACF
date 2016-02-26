83915d459d4911b04a55a3c041551d8b3a42f85c
j1990grossmann <Johannes.Grossmann@oeaw.ac.at>
2016-02-26 09:58:19 +0100

    tesdt

--- Plugin/SCPISrc/Hameg4040.cc
+++ Plugin/SCPISrc/Hameg4040.cc
@@ -90,8 +90,8 @@ int main(int argc, char* argv[])
 		h.OutPutGen(1);
 		std::cout<<"V1\tI1\tV2\tI2\tV3\tI3\tV4\tI4"<<std::endl;
 		timer.start();
-		for(int i=0;i<100;i++)
-			h.MeasAllA();
+// 		for(int i=0;i<100;i++)
+// 			h.MeasAllA();
 		timer.stop();
 		timer.show("Version A");
 		timer.start();
