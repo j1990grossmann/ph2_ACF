363e285af118ff11e78a54f3a86e8c5e94ccd5b4
j1990grossmann <Johannes.Grossmann@oeaw.ac.at>
2016-02-26 10:06:47 +0100

    test

--- Plugin/SCPISrc/Hameg4040.cc
+++ Plugin/SCPISrc/Hameg4040.cc
@@ -95,12 +95,12 @@ int main(int argc, char* argv[])
 		timer.stop();
 		timer.show("Version A");
 		timer.start();
-		for(int i=0;i<100;i++)
+// 		for(int i=0;i<100;i++)
 			h.MeasAllB();
 		timer.stop();
 		timer.show("Version B");
 		timer.start();
-		for(int i=0;i<100;i++)
+// 		for(int i=0;i<100;i++)
 			h.MeasAllC();
 		timer.stop();
 		timer.show("Version C");
