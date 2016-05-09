ffefcfe7adf5a39abee1644ca54f1867b0405135
Johannes Grossmann <Johannes.Grossmann@cern.ch>
2016-05-09 13:19:32 +0200

    test

--- Plugin/SCPITools/Initserial.h
+++ Plugin/SCPITools/Initserial.h
@@ -1,55 +1,59 @@
-  /*
-  * File:   Hameg4040.h
-  * Author: Johannes Grossmann
-  * Distributed under the Boost Software License, Version 1.0.
-  * Created on February 19th, 2015, 10:46 AM
-  */
-  
-  #ifndef INITSERIAL_H
-  #define	INITSERIAL_H
-  
-  #include <chrono>
-  #include <cstdlib>
-  #include <iostream>
-  #include <memory>
-  #include <string>
-  #include <thread>
-  #include <vector>
-  #include "../SCPIUtils/pugixml.hpp"
-  #include "../SCPIUtils/ConsoleColor.h"
-  #include "AsyncSerial.h"
-  #include "BufferedAsyncSerial.h"
-  
-  using namespace std;
-  // using namespace BufferedAsyncSerial;
-  
-  namespace INITSERIAL{
-    typedef std::map<std::string, std::string>    SerialSettingsMap;
-    
-    class Serial
-    {
-      
-    public:
-      Serial(const std::string& pFilename) : fFilename(pFilename) {
-// 	ParseSettingsXML(fFilename, std::cout);
-	serial=make_shared<BufferedAsyncSerial>();
-// 	this->Initialise(gSerialSettingsmap);
-// 	this->EmptyBuffer();
-      }
-      ~Serial(){
-      }
-      // methods
-      // 		void Initialise(AsyncSerial &serial, SerialSettingsMap &fSerialSettingMap);  // wants to be called after SystemController::ReadHW, ReadSettings
-      std::string endline;
-    protected:
-      void EmptyBuffer();  // wants to be called after SystemController::ReadHW, ReadSettings
-      void Initialise(SerialSettingsMap &fSerialSettingMap);  // wants to be called after SystemController::ReadHW, ReadSettings
-      void ParseSettingsXML(const string& pFilename, ostream& os);
-      string fFilename;
-      shared_ptr<BufferedAsyncSerial>serial;
-      SerialSettingsMap gSerialSettingsmap;
-    private:
-    };
-  }
-  
-  #endif 
+/*
+ * File:   Hameg4040.h
+ * Author: Johannes Grossmann
+ * Distributed under the Boost Software License, Version 1.0.
+ * Created on February 19th, 2015, 10:46 AM
+ */
+
+#ifndef INITSERIAL_H
+#define	INITSERIAL_H
+
+#include <chrono>
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+#include <vector>
+#include "../SCPIUtils/pugixml.hpp"
+#include "../SCPIUtils/ConsoleColor.h"
+#include "AsyncSerial.h"
+#include "BufferedAsyncSerial.h"
+
+using namespace std;
+// using namespace BufferedAsyncSerial;
+
+namespace INITSERIAL{
+	typedef std::map<std::string, std::string>    SerialSettingsMap;
+	
+	class Serial
+	{
+		
+	public:
+		Serial(const std::string& pFilename) : fFilename(pFilename) {
+			// 	ParseSettingsXML(fFilename, std::cout);
+			serial=make_shared<BufferedAsyncSerial>();
+			// 	   this->Serial::ParseSettingsXML(pFilename,std::cout);
+			this->Serial::Initialise(this->Serial::gSerialSettingsmap);
+			this->Serial::EmptyBuffer();
+			this->Initialise(gSerialSettingsmap);
+			
+			// 	this->EmptyBuffer();
+		}
+		~Serial(){
+		}
+		// methods
+		// 		void Initialise(AsyncSerial &serial, SerialSettingsMap &fSerialSettingMap);  // wants to be called after SystemController::ReadHW, ReadSettings
+		std::string endline;
+	protected:
+		void EmptyBuffer();  // wants to be called after SystemController::ReadHW, ReadSettings
+		void Initialise(SerialSettingsMap &fSerialSettingMap);  // wants to be called after SystemController::ReadHW, ReadSettings
+		void ParseSettingsXML(const string& pFilename, ostream& os);
+		string fFilename;
+		shared_ptr<BufferedAsyncSerial>serial;
+		SerialSettingsMap gSerialSettingsmap;
+	private:
+	};
+}
+
+#endif 
