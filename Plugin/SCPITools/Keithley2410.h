01269258373fb521ad010f875fa7a9ccc987ac20
Johannes Grossmann <Johannes.Grossmann@cern.ch>
2016-05-09 11:37:34 +0200

    test

--- Plugin/SCPITools/Keithley2410.h
+++ Plugin/SCPITools/Keithley2410.h
@@ -35,8 +35,8 @@ namespace KEITHLEY2410{
  {
  public:
   Keithley2410(const std::string& pFilename){
-   fFilename=pFilename;
-   std::cout<<this->Serial::fFilename<<std::endl;
+   this->Serial::fFilename=pFilename;
+   std::cout<<this->Serial::fFilename<<"\tfilename"<<std::endl;
    endline = "\n";
   }
   
