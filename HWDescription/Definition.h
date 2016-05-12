2bda2c88f42cd77a9da4bf89fe0a1d10429356f1
Johannes Grossmann <Johannes.Grossmann@cern.ch>
2016-05-12 14:24:23 +0200

    Update the Definition.h Constants to cope with the MPA Data Formats

--- HWDescription/Definition.h
+++ HWDescription/Definition.h
@@ -63,3 +63,43 @@
 #define OFFSET_CBCSTUBDATA        264+23  //LAST BIT
 #define WIDTH_CBCSTUBDATA         12
 //------------------------------------------------------------------------------
+//MPAL Event
+#define MPAL_EVENT_SIZE_72   96 // 96 72bit words per MPA
+#define MPAL_WORD_SIZE       72 // 96 72bit words per MPA
+// Asynchrounous
+#define MPAL_ASYNCHROUNOUS_HEADER_OFFSET                      0
+#define MPAL_ASYNCHROUNOUS_HEADER_WIDTH                       32
+#define MPAL_ASYNCHROUNOUS_RIPPLE_COUNTER_BITS                16
+#define MPAL_NCHANNELS                                        48
+// Synchrounous
+// MPA_BX_WIDTH
+#define MPAL_BX_WIDTH                           16
+// MPA_STUB_WORD
+#define MPAL_STUB_OFFSET_Z_POS                  0
+#define MPAL_STUB_WIDTH_Z_POS                   2
+#define MPAL_STUB_OFFSET_BENDING                2
+#define MPAL_STUB_WIDTH_BENDING                 5
+#define MPAL_STUB_OFFSET_POSITION               7
+#define MPAL_STUB_WIDTH_POSITION                7
+// MPA_CENTROID_WORD
+#define MPAL_CENTROID_Z_POS_OFFSET 0
+#define MPAL_CENTROID_Z_POS_WIDTH  2
+#define MPAL_CENTROID_POSITION_OFFSET  2
+#define MPAL_CENTROID_POSITION_OFFSET  6
+// Stub Finding Logic Mode OM 00
+#define MPAL_SF_MODE_STUBS_PER_BX              4
+#define MPAL_SF_MODE_BX_OFFSET                 52
+#define MPAL_SF_MODE_HEADER_OFFSET             68
+#define MPAL_SF_MODE_HEADER_WIDTH              4
+// No Processing Mode OM 11
+#define MPAL_NP_MODE_PIXEL_MATRIX_HITS_OFFSET 0
+#define MPAL_NP_MODE_PIXEL_MATRIX_HITS_WIDTH  48
+#define MPAL_NP_MODE_BX_OFFSET                48
+#define MPAL_NP_MODE_HEADER_OFFSET            64
+#define MPAL_NP_MODE_HEADER_WIDTH             8
+// Strip Emulator Mode OM 01
+#define MPAL_CENTROID_EXTRACTION_MODE_CENTROIDS_PER_BX  8
+#define MPAL_CENTROID_EXTRACTION_MODE_BX_OFFSET         56
+// Centroid Extraction Mode OM 11
+
+//------------------------------------------------------------------------------
